f83f7d647c933c87dd65aaa03a2ccd0ae517f399
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-06-16 11:29:14 -0400

    ugh

--- manifests/node/service.pp
+++ manifests/node/service.pp
@@ -4,7 +4,7 @@ class slurm::node::service {
   service { 'slurmd':
     ensure => $slurm::slurm_service_ensure,
     enable => $slurm::slurm_service_enable,
-    #name       => 'slurm',
+    name       => 'slurm',
     #hasstatus  => false,
     #hasrestart => true,
     #pattern    => "slurmd -f ${slurm::slurm_conf_path}",
