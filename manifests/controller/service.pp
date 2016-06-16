fdb47b87f4b1a9c8ced9fc31c996b2679b514935
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-06-16 12:51:57 -0400

    lets use real service names

--- manifests/controller/service.pp
+++ manifests/controller/service.pp
@@ -4,7 +4,6 @@ class slurm::controller::service {
   service { 'slurmctld':
     ensure     => $slurm::slurm_service_ensure,
     enable     => $slurm::slurm_service_enable,
-    name       => 'slurm',
     hasstatus  => false,
     hasrestart => true,
     pattern    => "slurmctld -f ${slurm::slurm_conf_path}",
