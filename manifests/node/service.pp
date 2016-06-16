6b66eb276ce2f004cee097f39ae411f7f2f06bca
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-06-16 12:31:30 -0400

    lint

--- manifests/node/service.pp
+++ manifests/node/service.pp
@@ -2,12 +2,12 @@
 class slurm::node::service {
 
   service { 'slurmd':
-    ensure => $slurm::slurm_service_ensure,
-    enable => $slurm::slurm_service_enable,
-    name       => 'slurm',
+    ensure  => $slurm::slurm_service_ensure,
+    enable  => $slurm::slurm_service_enable,
+    name    => 'slurm',
     #hasstatus  => false,
     #hasrestart => true,
-    pattern    => "slurmd -f ${slurm::slurm_conf_path}",
+    pattern => "slurmd -f ${slurm::slurm_conf_path}",
   }
 
 }
